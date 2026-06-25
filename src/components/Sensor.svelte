cfe90b2265ffd902741fe179e160bf1a474469e4
nayssam.benessalah.etu <nayssam.benessalah.etu@univ-lille.fr>
2026-06-25 11:39:44 +0200

    implementing Sensor component

---
+++ src/components/Sensor.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let {name,value, unit, alert_level}=$props();
+</script>
+<div class="capteur">
+  <h3>{name}</h3>
+  <p>{value.toFixed(2)} {unit}</p>
+  <p class="alert_level">Alert_level : {alert_level}</p>
+</div>
